a8ddf944f67e1c5fca3d4dbf3a034489e1b69735
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2017-12-25 15:39:31 +0200

    add policy_one
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

---
+++ policy/one.c
@@ -0,0 +1,40 @@
+#include "policy_api.h"
+#include "icd_log.h"
+
+static void
+icd_policy_one_connected(struct icd_policy_request *network,
+                         GSList *existing_connections, gpointer *private)
+{
+  icd_policy_nw_close_fn nw_close = (icd_policy_nw_close_fn)*private;
+  GSList *l;
+
+  ILOG_INFO("policy one got connected notification");
+
+  for (l = existing_connections; l; l = l->next)
+  {
+    struct icd_policy_request *request = (struct icd_policy_request *)l->data;
+
+    if (request != network)
+    {
+      ILOG_INFO("policy one disconnecting connection %p", request);
+      nw_close(request);
+    }
+    else
+      ILOG_INFO("policy one found the same connection");
+  }
+}
+
+void
+icd_policy_init(struct icd_policy_api *policy_api,
+                icd_policy_nw_add_fn add_network,
+                icd_policy_request_merge_fn merge_requests,
+                icd_policy_request_make_new_fn make_request,
+                icd_policy_scan_start_fn scan_start,
+                icd_policy_scan_stop_fn scan_stop,
+                icd_policy_nw_close_fn nw_close,
+                icd_policy_network_priority_fn priority,
+                icd_policy_service_module_check_fn srv_check)
+{
+  policy_api->private = nw_close;
+  policy_api->connected = icd_policy_one_connected;
+}

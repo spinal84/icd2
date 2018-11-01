a8a33b74b56adf93f74ca65aea1afa5f8484f956
Arthur Demchenkov <spinal.by@gmail.com>
2018-11-01 21:16:53 +0300

    Modify policy_nw_disconnect.c according to API doc

--- policy/policy_nw_disconnect.c
+++ policy/policy_nw_disconnect.c
@@ -4,50 +4,94 @@
 #include "policy_api.h"
 #include "icd_log.h"
 
-#define ICD_POLICY_NW_DISCONNECT_GCONF ICD_GCONF_SETTINGS "/policy/policy_nw_disconnect/"
+#define POLICY_NW_DISCONNECT_CANCELS_ALWAYS_ONLINE_GCONF_PATH \
+	ICD_GCONF_SETTINGS "/policy/policy_nw_disconnect/cancel_always_online"
+#define POLICY_NW_DISCONNECT_USER_REFCOUNT_GCONF_PATH \
+	ICD_GCONF_SETTINGS "/policy/policy_nw_disconnect/user_refcount"
+
+static gboolean
+policy_nw_disconnect_cancel_always_online()
+{
+  GError *error = NULL;
+  GConfClient *gconf = gconf_client_get_default();
+  GConfValue *val = gconf_client_get(
+      gconf, POLICY_NW_DISCONNECT_CANCELS_ALWAYS_ONLINE_GCONF_PATH, &error);
+  g_object_unref(gconf);
+
+  if (G_VALUE_HOLDS_BOOLEAN(val) && !error)
+  {
+    gboolean always_online = gconf_value_get_bool(val);
+    gconf_value_free(val);
+    return always_online;
+  }
+  else if (!error)
+    ILOG_DEBUG("policy nw disconnect cancel always online boolean is not set");
+  else
+  {
+    ILOG_DEBUG("policy nw disconnect cancel always online is not set, "
+	       "error '%s'", error->message);
+    g_error_free(error);
+  }
+
+  if (val)
+    gconf_value_free(val);
+
+  return FALSE;
+}
+
+static void
+policy_nw_disconnect_unset_always_online(void)
+{
+  GConfClient *gconf = gconf_client_get_default();
+  ILOG_DEBUG("policy nw disconnect disabling always online search interval");
+  gconf_client_set_int(gconf, ICD_GCONF_NETWORK_SEARCH_INTERVAL, 0, NULL);
+  g_object_unref(gconf);
+}
+
+static gboolean
+policy_nw_disconnect_user_refcount(void)
+{
+  GError *error = NULL;
+  GConfClient *gconf = gconf_client_get_default();
+  GConfValue *val = gconf_client_get(
+      gconf, POLICY_NW_DISCONNECT_USER_REFCOUNT_GCONF_PATH, &error);
+  g_object_unref(gconf);
+
+  if (G_VALUE_HOLDS_BOOLEAN(val) && !error)
+  {
+    gboolean user_refcount = gconf_value_get_bool(val);
+    gconf_value_free(val);
+    return user_refcount;
+  }
+  else if (!error)
+    ILOG_DEBUG("policy nw disconnect refcounting boolean is not set");
+  else
+  {
+    ILOG_DEBUG("policy nw disconnect refcounting is not set, error '%s'",
+	       error->message);
+    g_error_free(error);
+  }
+
+  if (val)
+    gconf_value_free(val);
+
+  return FALSE;
+}
 
 static enum icd_policy_status
-icd_policy_nw_disconnect_disconnect(struct icd_policy_request *network,
-                                    gint reference_count,
-                                    GSList *existing_connections,
-                                    gpointer *privatx)
+policy_nw_disconnect(struct icd_policy_request *network,
+		     gint reference_count,
+		     GSList *existing_connections,
+		     gpointer *private)
 {
-  GConfClient *gconf;
-  GConfValue *val;
-
-  GError *error;
-
   if (!reference_count)
   {
-    error = NULL;
-    gconf = gconf_client_get_default();
-    val = gconf_client_get(gconf,
-                           ICD_POLICY_NW_DISCONNECT_GCONF "user_refcount",
-                           &error);
-    g_object_unref(gconf);
-
-    if (G_VALUE_HOLDS_BOOLEAN(val) && !error)
+    if (policy_nw_disconnect_user_refcount())
     {
-      gboolean user_refcount = gconf_value_get_bool(val);
-      gconf_value_free(val);
-
-      if (user_refcount)
-      {
-        ILOG_INFO("policy nw disconnect reference counting turned on, disconnecting since no apps active");
-        return ICD_POLICY_ACCEPTED;
-      }
+      ILOG_INFO("policy nw disconnect reference counting turned on, "
+		"disconnecting since no apps active");
+      return ICD_POLICY_ACCEPTED;
     }
-    else if (!error)
-      ILOG_DEBUG("policy nw disconnect refcounting boolean is not set");
-    else
-    {
-      ILOG_DEBUG("policy nw disconnect refcounting is not set, error '%s'",
-                 error->message);
-      g_error_free(error);
-    }
-
-    if (val)
-      gconf_value_free(val);
   }
 
   if (reference_count >= 0)
@@ -60,48 +104,22 @@ icd_policy_nw_disconnect_disconnect(struct icd_policy_request *network,
   ILOG_INFO("policy nw disconnect reference count %d, disconnect from UI",
             reference_count);
 
-  error = NULL;
-  gconf = gconf_client_get_default();
-  val = gconf_client_get(gconf,
-                         ICD_POLICY_NW_DISCONNECT_GCONF "cancel_always_online",
-                         &error);
-  g_object_unref(gconf);
-
-  if (G_VALUE_HOLDS_BOOLEAN(val) && !error)
-  {
-    if (gconf_value_get_bool(val))
-    {
-      gconf = gconf_client_get_default();
-      ILOG_DEBUG("policy nw disconnect disabling always online search interval");
-      gconf_client_set_int(gconf, ICD_GCONF_NETWORK_SEARCH_INTERVAL, 0, NULL);
-      g_object_unref(gconf);
-    }
-  }
-  else if (!error)
-    ILOG_DEBUG("policy nw disconnect cancel always online boolean is not set");
-  else
-  {
-    ILOG_DEBUG("policy nw disconnect cancel always online is not set, error '%s'",
-               error->message);
-    g_error_free(error);
-  }
-
-  if (val)
-    gconf_value_free(val);
+  if (policy_nw_disconnect_cancel_always_online())
+    policy_nw_disconnect_unset_always_online();
 
   return ICD_POLICY_ACCEPTED;
 }
 
 void
 icd_policy_init(struct icd_policy_api *policy_api,
-                icd_policy_nw_add_fn add_network,
-                icd_policy_request_merge_fn merge_requests,
-                icd_policy_request_make_new_fn make_request,
-                icd_policy_scan_start_fn scan_start,
-                icd_policy_scan_stop_fn scan_stop,
-                icd_policy_nw_close_fn nw_close,
-                icd_policy_network_priority_fn priority,
-                icd_policy_service_module_check_fn srv_check)
+		icd_policy_nw_add_fn add_network,
+		icd_policy_request_merge_fn merge_requests,
+		icd_policy_request_make_new_fn make_request,
+		icd_policy_scan_start_fn scan_start,
+		icd_policy_scan_stop_fn scan_stop,
+		icd_policy_nw_close_fn nw_close,
+		icd_policy_network_priority_fn priority,
+		icd_policy_service_module_check_fn srv_check)
 {
-  policy_api->disconnect = icd_policy_nw_disconnect_disconnect;
+  policy_api->disconnect = policy_nw_disconnect;
 }
